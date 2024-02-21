f77b59775ee31caf52fe6ce963d5520100ad4177
Facundo Romano <facundo-romano@hotmail.com>
2024-02-21 17:40:34 -0300

    navbar fixed v2

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -114,7 +114,7 @@ function Navbar() {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel onClick={() => setMobileMenuOpen(false)}  className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="/" className="-m-0 p-0" >
               <span className="sr-only">Principios y Valores</span>
