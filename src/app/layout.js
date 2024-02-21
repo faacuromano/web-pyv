f7535e6196602c0d36fb222d8b0130f2da543a01
Facundo Romano <facundo-romano@hotmail.com>
2024-02-21 14:24:04 -0300

    analitycs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body clsassName={inter.className}>
         <Navbar></Navbar>
+        <Analytics></Analytics>
         {children}
         </body>
     </html>
