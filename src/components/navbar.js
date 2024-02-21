021c12d54b404273ed86b01cf8315891a6967a19
Facundo Romano <facundo-romano@hotmail.com>
2024-02-21 17:02:39 -0300

    navbar fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -142,18 +142,6 @@ function Navbar() {
                           aria-hidden="true"
                         />
                       </Disclosure.Button>
-                      <Disclosure.Panel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel>
                     </>
                   )}
                 </Disclosure>
