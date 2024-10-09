d29046bd59bd4cc6ccec4be04f5fa2114bc79636
facundo <facundo-romano@hotmail.com>
2024-10-09 12:07:21 -0300

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,35 +1,40 @@
 import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const roboto = Roboto_Condensed({
   subsets: ['latin'],
-  weight: ['100', '300','400', '500', '700'], // Puedes agregar más pesos si los necesitas
+  weight: ['100', '300', '400', '500', '700'], // Puedes agregar más pesos si los necesitas
 });
 
 export const metadata = {
   title: {
     default: "Inicio - Principios y Valores",
-    template: "%s - Principios y Valores"
+    template: "%s - Principios y Valores",
   },
   description: "Partido principios y valores - Santa Fe",
   twitter: {
-    card: "summary_large_image"
-  }
-  
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-        <body className={roboto.className}>
-        <Navbar></Navbar>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body className={roboto.className}>
+        <Navbar />
         {children}
-        <Analytics></Analytics>
-        <SpeedInsights></SpeedInsights>
-        </body>
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
