b8bf24a490e6101299eac5afe65533b3a5c6cc68
facundo <facundo-romano@hotmail.com>
2024-12-12 15:17:04 -0300

    ceo update

--- src/app/nosotros/politica-y-actualidad/card.js
+++ src/app/nosotros/politica-y-actualidad/card.js
@@ -3,12 +3,12 @@ import React from 'react';
 import Image from 'next/image';
 const ActividadCard = ({ titulo, subtitulo, texto, imagen }) => {
   return (
-    <div className="p-4 bg-blue-50 rounded shadow hover:shadow-lg transition">
-      <Image width={300} height={300} src={imagen} alt={titulo} className="w-full h-40 object-cover rounded-t" />
-      <div className="p-4">
+    <div className="shadow-sm bg-zinc-300 	border-solid border-gray-500  border	 rounded-lg rounded shadow hover:shadow-lg transition">
+      <Image width={300} height={300} src={imagen} alt={titulo} className="shadow-sm w-full h-40 object-cover rounded-t-lg" />
+      <div className=" p-4">
         <h2 className="text-lg font-semibold text-blue-700">{titulo}</h2>
-        <span className="text-sm text-red-600 font-semibold">{subtitulo}</span>
-        <p className="text-gray-600 mt-2">{texto}</p>
+        <span className="text-sm text-red-900 font-semibold">{subtitulo}</span>
+        <p className="text-gray-800 mt-2">{texto}</p>
       </div>
     </div>
   );
