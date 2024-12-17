6d3fe250c9f85b1a868add630d161819f045de13
facundo <facundo-romano@hotmail.com>
2024-12-17 13:14:16 -0300

    canonical fixed in part

--- src/app/arbol/rama-politica/layout.js
+++ src/app/arbol/rama-politica/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <div>
         {children}
-        <link rel="canonical" href="https://www.principiosyvaloressantafe.com.ar/arbol"></link>
+        <link rel="canonical" href="https://www.principiosyvaloressantafe.com.ar/arbol/rama-politica"></link>
 
     </div>
   );
